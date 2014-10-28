b01a972462f618edd5ff234dc4a4470b7e5292b3
Ian Wang <iwang@fonality.com>
2014-10-28 01:54:49 -0400

    init

---
+++ chessdemo/app/controllers/HelloWorldApp.java
@@ -0,0 +1,42 @@
+/*
+ * (C) Copyright 2014 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+package controllers;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import play.libs.Akka;
+import play.libs.F.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.mvc.WebSocket;
+import scala.concurrent.duration.Duration;
+import views.html.index;
+import akka.actor.ActorRef;
+import akka.actor.Cancellable;
+import akka.actor.Props;
+
+import org.kurento.client.factory.KurentoClient;
+
+public class HelloWorldApp extends Controller {
+
+	public static Result startDemo(){
+		
+		HelloWorldAppInst demo_app = new HelloWorldAppInst();
+		
+		RequestBody body = request().body();
+	    String textBody = body.asText();
+		String sdpAnswer = demo_app.processRequest(textBody);
+		return ok(sdpAnswer);
+	}
+}
---
+++ chessdemo/app/controllers/HelloWorldAppInst.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import java.io.IOException;
+
+import org.kurento.client.MediaPipeline;
+import org.kurento.client.WebRtcEndpoint;
+import org.kurento.client.factory.KurentoClient;
+
+public class HelloWorldAppInst {
+	
+	final static String DEFAULT_KMS_WS_URI = "ws://localhost:8888/kurento";
+	
+	private KurentoClient kurento;
+	
+	public void HelloWorldAppInst(){
+		kurento = KurentoClient.create(System.getProperty("kms.ws.uri",
+				DEFAULT_KMS_WS_URI));	
+	}
+
+	public String processRequest(String sdpOffer)
+			throws IOException {
+		
+		// Media Logic
+		MediaPipeline pipeline = kurento.createMediaPipeline();
+		WebRtcEndpoint webRtcEndpoint = new WebRtcEndpoint.Builder(pipeline)
+				.build();
+		webRtcEndpoint.connect(webRtcEndpoint);
+
+		// SDP negotiation (offer and answer)
+		String responseSdp = webRtcEndpoint.processOffer(sdpOffer);
+		return responseSdp;
+	}
+}
